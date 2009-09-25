6ed66fa44d3e26090702bdc4ede7fdd79f249c3f
Mason Chua <mason_chua@aftran.com>
2009-09-24 22:10:07 -0400

    monomial and lexicon types.

--- Type3LearnerTypes.ml
+++ Type3LearnerTypes.ml
@@ -21,3 +21,18 @@ type feature =
 
 (* Unfortunately, we type each feature's name three different times.  I'd like
  * a better way to define a feature and its allowed values in one go. *)
+
+type monomial = feature list
+(* We'll maybe use "feature list" or "feature set" of "feature hashtable". *)
+
+(* A monomial is maximal iff it contains one feature for each feature
+ * constructor.  (Easy function to write.)  *)
+
+type lexicon = (morph * monomial) list
+(* It'd be nice if I could encode a specific type for "maximal monomial" to
+ * enforce that a lexicon's elements can only have maximal monomials.
+ *
+ * And again, we might want the lexicon to be a (morph * monomial) set or a
+ * (morph * monomial, bool) hashtable. *)
+
+(* I haven't done Lex, BR and T yet. *)
