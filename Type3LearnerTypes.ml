87247086a8bfb14db4a0f92000139dcf8597dd2e
Mason Chua <mason_chua@aftran.com>
2009-10-07 21:33:55 -0400

    Actually, a lexicon is a different kind of table:
    It maps morphs to [a table that maps integers to monomials].
    This way we can more easily iterate over homonyms of each morph.

--- Type3LearnerTypes.ml
+++ Type3LearnerTypes.ml
@@ -17,7 +17,10 @@ end)
 
 type monomial = FSet.t
 
-type lexicon = ((morph*int),  monomial) Hashtbl.t
+(* A lexicon is a table that maps morphs to tables that map integers to
+ * monomials.  In other words, it lets each morph have multiple meanings
+ * (=monomials), which are indexed by integers. *)
+type lexicon = (morph, (int, monomial) Hashtbl.t) Hashtbl.t
 
 type blocker = (morph*int) * (morph*int)
 (* Might restructure the lexicon as a map so we can do something like
