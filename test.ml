684b9fa12dd0ecc6c390a3272f30c1375e5c4c50
Mason Chua <mason_chua@aftran.com>
2010-07-03 19:24:15 -0400

    Made it a functor.  Added 2string functions.

--- test.ml
+++ test.ml
@@ -1,30 +1,18 @@
-open Type3Learner
+module MyLearner = Type3Learner.Make(struct
+        type feature = string*string
+        type morph = string
 
-(* ------- UTILITIES---------*)
+        let compare_features = compare
+        let compare_morphs = compare
 
-let printSet m = let rec printFeat = function
-      |[] -> print_newline()
-      |(feat,value)::t -> (print_string feat; print_string":"; print_string value; printFeat t) 
-    in printFeat (FSet.elements m)
+        let feature2string = function (a,b) -> b ^ a
+end)
 
-(* printing the lexicon *)
-let printLexicon lexicon = 
-  let printMon key monomial = 
-    print_int key;
-    print_string " - ";
-    printSet monomial
-  in
-  let printLexeme key lexeme = 
-    print_string key; 
-    IntMap.iter printMon lexeme
-  in Lexicon.iter printLexeme lexicon;;
-
-(*KP test*)
 (* define a paradigm over 2 binary features A and B*)
-let m1 = monomial [("A","+"); ("B","-")];;
-let m2 = monomial [("A","+"); ("B","+")];;
-let m3 = monomial [("A","-"); ("B","+")];;
-let m4 = monomial [("A","-"); ("B","-")];;
+let m1 = list2monomial [("A","+"); ("B","-")];;
+let m2 = list2monomial [("A","+"); ("B","+")];;
+let m3 = list2monomial [("A","-"); ("B","+")];;
+let m4 = list2monomial [("A","-"); ("B","-")];;
 
 let text1 = [("y",m1);("x",m2);("y",m3);("x",m4)];;
 let text2 = [("y",m1);("y",m2);("y",m3);("x",m4)];;
@@ -82,11 +70,11 @@ assert ( not ( DG.is_empty myBR ) );;
 assert ( not ( DG.is_empty correctBR ) );;
 assert  ( myBR = correctBR );;
 
-let e1 = monomial [("A","+"); ("B","+")]
-let e2 = monomial [("A","-"); ("B","+")]
-let e3 = monomial [("A","+"); ("B","-")]
-let e4 = monomial [("A","-"); ("B","-")]
-let eA = monomial [("A","+")]
+let e1 = list2monomial [("A","+"); ("B","+")]
+let e2 = list2monomial [("A","-"); ("B","+")]
+let e3 = list2monomial [("A","+"); ("B","-")]
+let e4 = list2monomial [("A","-"); ("B","-")]
+let eA = list2monomial [("A","+")]
 
 let mySeenTable = table [
         (e1, [("x",4)]);
@@ -108,22 +96,22 @@ assert ( not ( DG.is_empty myBR2 ) );;
 assert ( myBR2 = correctBR2 );;
 
 
-let m1 = monomial [("A","+"); ("B","-")]
-let m2 = monomial [("B","-"); ("G","+"); ("A","-")]
-let m3 = monomial [("C","+"); ("A","-")]
-let m4 = monomial [("D","+"); ("I","-"); ("J","-"); ("K","-")]
-let m5 = monomial [("E","+"); ("A","-")]
-let m6 = monomial [("A","-"); ("E","+")]
-let m7 = monomial [("A","-"); ("E","+")];;
+let m1 = list2monomial [("A","+"); ("B","-")]
+let m2 = list2monomial [("B","-"); ("G","+"); ("A","-")]
+let m3 = list2monomial [("C","+"); ("A","-")]
+let m4 = list2monomial [("D","+"); ("I","-"); ("J","-"); ("K","-")]
+let m5 = list2monomial [("E","+"); ("A","-")]
+let m6 = list2monomial [("A","-"); ("E","+")]
+let m7 = list2monomial [("A","-"); ("E","+")];;
 
 assert  (not (m1 = m2));;
 assert  (m1 = m1);;
 assert  (m6 = m7);;
 
-let e = monomial [("A","+"); ("B","-"); ("C","+"); ("D","-"); ("E","-"); ("A","-")];;
+let e = list2monomial [("A","+"); ("B","-"); ("C","+"); ("D","-"); ("E","-"); ("A","-")];;
 
 assert  (compare_dissim_to e m1 m3 = -(compare 2 2));;
-assert  (compare_dissim_to e m2 (monomial []) = -(compare 2 0));;
+assert  (compare_dissim_to e m2 (list2monomial []) = -(compare 2 0));;
 
 let ms = [(1,m1); (2,m2); (3,m3); (4,m5); (5,m4); (6,m6); (7,m7)]
 
@@ -135,7 +123,7 @@ let ms2 = [(1,m5); (2,m1); (3,m4);];;
 assert  ([(2,m1); (1,m5); (3,m4)] = sort_dissim_to e ms2)
 
 (* Test a first step in learning: *)
-let mn = monomial [("A","-"); ("B","-"); ("C","-")]
+let mn = list2monomial [("A","-"); ("B","-"); ("C","-")]
 let (lex, v, br, s, p) = learn
                 Lexicon.empty G.empty DG.empty Table.empty Table.empty "hello" mn
 let expectedLex = update_lex Lexicon.empty "hello" 1 mn;;
