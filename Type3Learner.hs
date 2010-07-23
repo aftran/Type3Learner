e350f1df0735bac7a0e1f9a1d70f4096a9bf34f1
Mason Chua <mason_chua@aftran.com>
2010-07-23 15:57:34 -0400

    Added matches function.  Rearrangement.

--- Type3Learner.hs
+++ Type3Learner.hs
@@ -1,7 +1,8 @@
 module Type3Learner (
      Monomial,
      Lexicon,
-     monomialFromList)
+     monomialFromList,
+     matches)
 where
 
 import Data.Set
@@ -9,14 +10,19 @@ import Data.Map
 
 type Monomial f = Set f
 
+monomialFromList :: (Ord f) => [f] -> Monomial f
+monomialFromList = Data.Set.fromList
+
 -- A morph with an integer subscript.
 data Mi w = Mi w Int
+     deriving (Eq, Ord)
 
 type Lexicon w f = Map (Mi w) [Monomial f]
 
-type Miset w = Set (Mi w)
+type Table w f = Map (Monomial f) (Set (Mi w))
 
-type Table w f = Map (Monomial f) (Miset w)
-
-monomialFromList :: (Ord f) => [f] -> Monomial f
-monomialFromList = Data.Set.fromList
+-- In terms of Type3learner, matches e t = the morphs predicted to appear in
+-- environment e, according to table t.
+-- matches e t = the union of all values of t whose key is a subset of e.
+-- matches :: (Ord f, Ord w) => Monomial f -> Table w f -> Set (Mi w)
+matches e = Data.Set.unions . Data.Map.elems . Data.Map.filterWithKey (\k _ -> e `isSubsetOf` k)
