d0fa422b8ad86677d0d510a11ff5e4aed9aac2cb
Mason Chua <mason_chua@aftran.com>
2010-07-24 16:59:47 -0400

    Added addToLexicon.

--- Type3Learner.hs
+++ Type3Learner.hs
@@ -1,28 +1,39 @@
-module Type3Learner (
-     Monomial,
-     Lexicon,
-     monomialFromList,
-     matches)
-where
+module Type3Learner where
 
-import Data.Set
-import Data.Map
+import qualified Data.Set as S
+import qualified Data.Map as M
 
-type Monomial f = Set f
+type Monomial f = S.Set f
 
 monomialFromList :: (Ord f) => [f] -> Monomial f
-monomialFromList = Data.Set.fromList
+monomialFromList = S.fromList
 
 -- A morph with an integer subscript.
 data Mi w = Mi w Int
-     deriving (Eq, Ord)
+    deriving (Eq, Ord)
 
-type Lexicon w f = Map (Mi w) [Monomial f]
+instance (Show w) => Show (Mi w) where
+    show (Mi x i) = show x ++ "_" ++ show i
 
-type Table w f = Map (Monomial f) (Set (Mi w))
+type Lexeme f = M.Map Int (Monomial f)
+
+type Lexicon w f = M.Map w (Lexeme f)
+
+type Table w f = M.Map (Monomial f) (S.Set (Mi w))
 
 -- In terms of Type3learner, matches e t = the morphs predicted to appear in
 -- environment e, according to table t.
 -- matches e t = the union of all values of t whose key is a subset of e.
--- matches :: (Ord f, Ord w) => Monomial f -> Table w f -> Set (Mi w)
-matches e = Data.Set.unions . Data.Map.elems . Data.Map.filterWithKey (\k _ -> e `isSubsetOf` k)
+matches :: (Ord f, Ord w) => Monomial f -> Table w f -> S.Set (Mi w)
+matches e = S.unions . M.elems . M.filterWithKey p
+  where p k _ = e `S.isSubsetOf` k
+
+-- addToLexicon lexicon morph index monomial = the lexicon with the added
+-- monomial (meaning) associated with the index of the morph.
+addToLexicon :: (Ord w) => Lexicon w f -> w -> Int -> Monomial f -> Lexicon w f
+addToLexicon lex morph index monomial = M.alter f morph lex
+  where f Nothing  = Just $ M.singleton index monomial
+        f (Just l) = Just $ M.insert index monomial l
+
+emptyLexicon :: Lexicon w f
+emptyLexicon = M.empty
--- test.hs
+++ test.hs
@@ -3,3 +3,7 @@ import Type3Learner
 
 data Feature = Bright | Dim
      deriving (Show, Eq, Ord)
+
+l2 = addToLexicon emptyLexicon "hi" 2 (monomialFromList [Dim])
+l3 = addToLexicon l2 "hi" 1 (monomialFromList [Dim, Bright])
+
