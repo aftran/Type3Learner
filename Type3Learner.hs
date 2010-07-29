f413d7ff8207253a36ac8d721f7d06be0dfc937f
Mason Chua <mason_chua@aftran.com>
2010-07-28 23:06:57 -0400

    Made Table values Set (Mi w) instead of [w] again.

--- Type3Learner.hs
+++ Type3Learner.hs
@@ -17,15 +17,15 @@ type Lexeme f = M.Map Int (Monomial f)
 
 type Lexicon w f = M.Map w (Lexeme f)
 
-type Table w f = M.Map (Monomial f) [Mi w]
+type Table w f = M.Map (Monomial f) (S.Set (Mi w))
 
 data Hypothesis w f br = Hypothesis (Lexicon w f) br
 
 -- In terms of Type3learner, matches e t = the morphs predicted to appear in
 -- environment e, according to table t.
 -- matches e t = the union of all values of t whose key is a subset of e.
-matches :: (Ord f, Ord w) => Monomial f -> Table w f -> [Mi w]
-matches e = concat . M.elems . M.filterWithKey f
+matches :: (Ord f, Ord w) => Monomial f -> Table w f -> S.Set (Mi w)
+matches e = S.unions . M.elems . M.filterWithKey f
   where f k _ = e `S.isSubsetOf` k
 
 -- addToLexicon lexicon morph index monomial = the lexicon with the added
@@ -42,10 +42,10 @@ emptyTable = M.empty
 
 -- addToTable tbl monomial morph index = the table with (Mi morph index) added
 -- to the set associated with monomial.
-addToTable :: (Ord f) => Table w f -> Monomial f -> w -> Int -> Table w f
+addToTable :: (Ord f, Ord w) => Table w f -> Monomial f -> w -> Int -> Table w f
 addToTable tbl monomial morph idx = M.alter f monomial tbl
   where m = Mi morph idx
-        f = Just . fromMaybe [m] . (fmap $ (:) m)
+        f = Just . fromMaybe (S.singleton m) . (fmap $ S.insert m)
 
 -- similarity s t = the cardinality of s intersected with t.
 similarity :: (Ord f) => (Monomial f) -> (Monomial f) -> Int
