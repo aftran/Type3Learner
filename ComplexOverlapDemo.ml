e1691dce166db1fc356cc31b308df91cdd2b0b5c
Mason Chua <mason_chua@aftran.com>
2010-08-13 15:36:45 -0400

    Test of weird_overlap.

---
+++ ComplexOverlapDemo.ml
@@ -0,0 +1,22 @@
+module L = Type3learner.Make(struct
+        type feature = string
+        type morph   = string
+
+        let compare_features = compare
+        let compare_morphs   = compare
+
+        let id o = o
+        let feature2string = id
+        let morph2string   = id
+end)
+
+let m1 =  L.list2monomial ["A1"; "B-"];; 
+let m2 =  L.list2monomial ["A2"; "B-"];; 
+let m3 =  L.list2monomial ["A3"; "B-"];; 
+let m4 =  L.list2monomial ["A1"; "B+"];;
+let m5 =  L.list2monomial ["A2"; "B+"];;
+let m6 =  L.list2monomial ["A3"; "B+"];;
+
+let text = [("y",m1);("y",m6);("w",m5);("x",m4);("y",m2)];; 
+
+L.type3learn_with_printing text;;
