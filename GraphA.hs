acbee703d649508e68fde923aa881313bd5f1d0d
Aftran <aftran@it5.irss.unc.edu>
2010-08-17 18:09:35 -0400

    Added GraphA.hasCycle.

--- GraphA.hs
+++ GraphA.hs
@@ -1,14 +1,17 @@
 module GraphA (
-    GraphA,
+    GraphA(..),
     GraphA.empty,
-    addEdge
+    addEdge,
+    hasCycle
 ) where
 -- TODO: This module might could be replaced by Data.Graph.EasyGrapher.
 
 import qualified Data.Graph.Inductive.Graph as GC
 import qualified Data.Graph.Inductive.Tree  as G
 import Data.Map as M
+import Data.Graph.Inductive.Basic (gsel, preorderF)
 import Data.Maybe
+import Data.Graph.Inductive.Query.DFS (reachable, dff)
 
 data GraphA a = GraphA (G.Gr a ()) (Map a GC.Node)
 
@@ -32,6 +35,18 @@ insNode a orig@(GraphA gr m) = fromMaybe def (fmap (const orig) (M.lookup a m))
           where newNode = head $ GC.newNodes 1 gr
                 m2      = M.insert a newNode m
                 gr2     = GC.insNode (newNode, a) gr
+
+-- True iff the GraphA has a cycle.
+hasCycle :: GraphA a -> Bool
+hasCycle (GraphA g _) = graphHasCycle g
+
+-- True iff the graph (G.Gr) has a cycle.
+graphHasCycle :: G.Gr a b -> Bool
+graphHasCycle g = not . Prelude.null . gsel f $ g
+  where f context = elem (GC.node' context) (reachableNotSelf context g)
+
+reachableNotSelf :: GC.Graph gr => GC.Context a b -> gr a b -> [GC.Node]
+reachableNotSelf c g = preorderF (dff (GC.suc' c) g)
 
 instance (Show a) => Show (GraphA a) where
     show (GraphA g n) = show g
